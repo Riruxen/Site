bc9bb50efaf9aa50e2fa8f1ec5d41c95c652fec4
Riruxen <rodi95241@gmail.com>
2026-05-08 21:05:35 +0200

    adding working buttons

--- app/routes1.py
+++ app/routes1.py
@@ -6,3 +6,33 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 @rout.route("/")
 def show():
     return render_template("haupt.html")
+@rout.route("/wien")
+def wien():
+    return render_template("haupt.html")
+@rout.route("/linz")
+def linz():
+    return render_template("haupt.html")
+@rout.route("/salzburg")
+def salzburg():
+    return render_template("haupt.html")
+
+@rout.route("/graz")
+def graz():
+    return render_template("haupt.html")
+@rout.route("/inssbruck")
+def inssbruck():
+    return render_template("haupt.html")
+@rout.route("/geschichte")
+def geschichte():
+    return render_template("haupt.html")
+
+@rout.route("/über")
+def über():
+    return render_template("haupt.html")
+
+@rout.route("/faq")
+def faq():
+    return render_template("haupt.html")
+
+
+
