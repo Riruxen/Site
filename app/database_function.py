50be80f976dcbcd2eeae04e5da1a5fb1c498b2d8
Riruxen <rodi95241@gmail.com>
2026-05-16 20:38:03 +0200

    improving functions for Ticket table

--- app/database_function.py
+++ app/database_function.py
@@ -1,7 +1,7 @@
 from sqlalchemy import delete, update
 from werkzeug.security import generate_password_hash, check_password_hash
 from create_db import engine, Session, Base
-from classes import User
+from classes import User , Ticket
 
 
 
@@ -82,3 +82,64 @@ def autoriz_check(name,password1):
                 return True
             else:
                 return False
+def ticket_add_db(place,uniqe_id):#working
+    person_ticket = Ticket(place = place, uniqe_id=uniqe_id)
+    with Session() as session:
+        if session.query(Ticket.ticket_id).filter_by(uniqe_id = uniqe_id).first() is not None:
+            return False
+        else:
+            session.add(person_ticket)
+            session.commit()
+            return True
+def ticket_delete_db(id):#50/50
+    with Session() as session:
+        if session.query(Ticket.ticket_id).filter_by(ticket_id=id).first():
+
+            delete_ticket= delete(Ticket).where(Ticket.ticket_id == id)
+            session.execute(delete_ticket)
+            session.commit()
+            return True
+        else:
+            return False
+def ticket_read1_db(id):# working!
+    with Session() as session:
+        find_ticket = session.query(Ticket).where(Ticket.ticket_id == id).first()
+        if find_ticket:
+            return find_ticket
+        else:
+            return "Error"
+def ticket_readall():#working
+    try:
+        with Session() as session:
+            ticket_all_data = session.query((Ticket.ticket_id,Ticket.owner,Ticket.uniqe_id,Ticket.place,Ticket.user_id)).all()
+            return ticket_all_data
+    except:
+        return None
+def ticket_readlast():#work
+    with Session() as session:
+        try:
+            last_data_ticket = session.query(Ticket.ticket_id,Ticket.owner,Ticket.uniqe_id,Ticket.place,Ticket.user_id).order_by(Ticket.ticket_id.desc()).first()
+            if last_data_ticket is not None:
+                return last_data_ticket
+            else:
+                return None
+        except:
+            return None
+
+def ticket_updatedb(place,uniqe_id):
+    try:
+        values = {}
+        if place:
+            values["place"] = place
+        if uniqe_id:
+            values["uniqe_id"] = uniqe_id
+        if not values:
+            return False
+        if uniqe_id and uniqe_id > 0:
+            with Session() as session:
+                up = update(Ticket).where(Ticket.uniqe_id == id).values(**values)
+                session.execute(up)
+                session.commit()
+            return True
+    except:
+            return False
