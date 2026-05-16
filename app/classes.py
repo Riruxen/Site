e69719b1fdabf29a2d62852c571e03ee821d22b3
Riruxen <rodi95241@gmail.com>
2026-05-16 20:25:29 +0200

    adding ticket table with relationship and ForeignKey

--- app/classes.py
+++ app/classes.py
@@ -1,23 +1,25 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import String,ForeignKey
+from sqlalchemy.orm import Mapped,mapped_column, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from create_db import Base
 class User(Base):
     __tablename__='user'
 
-    id:Mapped[int] = mapped_column(primary_key=True)
+    id:Mapped[int] = mapped_column(primary_key=True) 
     name:Mapped[str] = mapped_column(unique=True)
     password:Mapped[str] = mapped_column(String(255), nullable=False)
     email:Mapped[str] = mapped_column(unique=True)
     card_number:Mapped[str | None] = mapped_column(String(20))
     card_expiry: Mapped[str | None] = mapped_column(String(5))
+    buyed_tickets: Mapped[list['Ticket']]= relationship('owner')
     def __repr__(self):
         return f"{self.id}:{self.name}->"
 
 class Ticket(Base):
     __tablename__='tickets'
 
-    id:Mapped[int] = mapped_column(primary_key=True)
-    name:Mapped[str] = mapped_column()
+    ticket_id:Mapped[int] = mapped_column(primary_key=True)
+    user_id:Mapped[int] = mapped_column(ForeignKey("user.id"))
     place:Mapped[str] = mapped_column()
     uniqe_id:Mapped[int] = mapped_column(unique=True)
+    owner:Mapped[User] = relationship(back_populates='buyed_tickets')
